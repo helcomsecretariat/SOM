Fixed contribution normalizing
- removed pickle
- better error messages and warnings
- more comments

protect_baltic/src/__main__.py
@@ -11,28 +11,11 @@ url: 'https://github.com/helcomsecretariat/SOM/blob/main/protect_baltic/LICENCE'
 
 import toml
 import som.som_app as som_app
-from utilities import Timer, exception_traceback
+from utilities import Timer, exception_traceback, fail_with_message
 import os
-import pickle
 import pandas as pd
+import numpy as np
 import sys
-
-
-def p_save(data: object, path: str):
-    """
-    Saves the given data as a pickle object
-    """
-    with open(path, 'wb') as f:
-        pickle.dump(data, f)
-
-
-def p_load(path: str):
-    """
-    Loads pickle data
-    """
-    with open(path, 'rb') as f:
-        data = pickle.load(f)
-    return data
 
 
 def run(is_test: bool = False):
@@ -53,36 +36,24 @@ def run(is_test: bool = False):
                     if key in config['test_data'].keys():
                         config['input_data'][key] = config['test_data'][key]
     except Exception as e:
-        print('Could not load config file!')
-        exception_traceback(e)
-        return
+        fail_with_message('ERROR! Could not load config file!', e)
 
     #
     # do stuff
     #
     try:
-        data_path = 'data.p'
-        links_path = 'links.p'
-        if config['pickle'] and os.path.exists(data_path) and os.path.exists(links_path):
-            # load pickled data
-            data = p_load(data_path)
-            links = p_load(links_path)
-        else:
-            # Process survey data and read general input
-            data = som_app.process_input_data(config)
-            # Create links between core components
-            links = som_app.build_links(data)
-            if config['use_scenario']:
-                # Update activity contributions to scenario values
-                data['activity_contributions'] = som_app.build_scenario(data, config['scenario'])
-            # Create cases
-            data['cases'] = som_app.build_cases(data['cases'], links)
-            
-        if config['pickle']:
-            # save the data as pickle objects
-            p_save(data, data_path)
-            p_save(links, links_path)
-
+        if config['use_random_seed']:
+            np.random.seed(config['random_seed'])
+        # Process survey data and read general input
+        data = som_app.process_input_data(config)
+        # Create links between core components
+        links = som_app.build_links(data)
+        if config['use_scenario']:
+            # Update activity contributions to scenario values
+            data['activity_contributions'] = som_app.build_scenario(data, config['scenario'])
+        # Create cases
+        data['cases'] = som_app.build_cases(data['cases'], links)
+        # Run model
         data = som_app.build_changes(data, links)
 
         #
@@ -98,9 +69,9 @@ def run(is_test: bool = False):
             data['thresholds']['PR'].to_excel(writer, sheet_name='RequiredReductionsForGES', index=False)
 
     except Exception as e:
-        exception_traceback(e)
+        fail_with_message('ERROR! Something went wrong! Check traceback.', e)
     
-    print(f'\nProgram terminated after {timer.get_hhmmss()}')
+    print(f'\nProgram terminated successfully after {timer.get_hhmmss()}')
 
     return
 