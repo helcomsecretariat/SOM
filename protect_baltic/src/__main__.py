56a7db9b30b5ace7c1196d5a3947a4d977e6b78b
Camilo Hernández <blobbeliblob@gmail.com>
2025-03-24 10:41:14 +0200

    Update __main__.py

--- protect_baltic/src/__main__.py
+++ protect_baltic/src/__main__.py
@@ -20,7 +20,7 @@ import copy
 import shutil
 
 
-def run(config_file: str = None):
+def run(config_file: str = None, skip_sim: bool = False):
     log_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'log.txt')
     log = open(log_path, 'w')
 
@@ -46,7 +46,8 @@ def run(config_file: str = None):
     sim_res_path = os.path.join(os.path.dirname(export_path), 'sim_res')
     if os.path.exists(sim_res_path):
         for f in [x for x in os.listdir(sim_res_path) if x.endswith('.xlsx') and 'sim_res' in x]:
-            os.remove(os.path.join(sim_res_path, f))
+            if not skip_sim:
+                os.remove(os.path.join(sim_res_path, f))
     os.makedirs(sim_res_path, exist_ok=True)
     out_dir = os.path.join(os.path.dirname(export_path), 'output')
     if os.path.exists(out_dir): shutil.rmtree(out_dir)
@@ -60,6 +61,7 @@ def run(config_file: str = None):
         np.random.seed(config['random_seed'])
 
     # Process survey data and read general input
+    print('Loading input data...')
     try:
         print('Loading input data...', file=log)
         input_data = som_app.process_input_data(config)
@@ -67,45 +69,46 @@ def run(config_file: str = None):
         fail_with_message(f'ERROR! Something went wrong while processing input data! Check traceback.', e)
     
     print('Running simulations...')
-    display_progress(0)
-    for i in range(config['simulations']):
-        try:
-            display_progress((i + 1) / config['simulations'])
-            print(f'sim = {i}', file=log)
+    if not skip_sim:
+        display_progress(0)
+        for i in range(config['simulations']):
+            try:
+                display_progress((i + 1) / config['simulations'])
+                print(f'sim = {i}', file=log)
 
-            # Create links between core components
-            print('\tBuilding links between Measures, Activities, Pressures and States...', file=log)
-            data = som_app.build_links(copy.deepcopy(input_data))
+                # Create links between core components
+                print('\tBuilding links between Measures, Activities, Pressures and States...', file=log)
+                data = som_app.build_links(copy.deepcopy(input_data))
 
-            if config['use_scenario']:
-                # Update activity contributions to scenario values
-                print('\tApplying activity development scenario...', file=log)
-                data['activity_contributions'] = som_app.build_scenario(data, config['scenario'])
-            
-            # Create cases
-            print('\tBuilding cases...', file=log)
-            data = som_app.build_cases(data)
+                if config['use_scenario']:
+                    # Update activity contributions to scenario values
+                    print('\tApplying activity development scenario...', file=log)
+                    data['activity_contributions'] = som_app.build_scenario(data, config['scenario'])
+                
+                # Create cases
+                print('\tBuilding cases...', file=log)
+                data = som_app.build_cases(data)
 
-            # Run model
-            print('\tCalculating changes in environment...', file=log)
-            data = som_app.build_changes(data)
+                # Run model
+                print('\tCalculating changes in environment...', file=log)
+                data = som_app.build_changes(data)
 
-            #
-            # export results
-            #
-            print('\tExporting results...', file=log)
-            temp_path = os.path.join(os.path.dirname(export_path), 'sim_res', f'sim_res_{i}.xlsx')
-            with pd.ExcelWriter(temp_path) as writer:
-                data['pressure_levels'].to_excel(writer, sheet_name='PressureLevels', index=False)
-                data['total_pressure_load_levels'].to_excel(writer, sheet_name='TPLLevels', index=False)
-                data['total_pressure_load_reductions'].to_excel(writer, sheet_name='TPLReductions', index=False)
-                data['thresholds']['PR'].to_excel(writer, sheet_name='RequiredReductionsForGES', index=False)
-                data['measure_effects'].to_excel(writer, sheet_name='MeasureEffects', index=False)
-                data['activity_contributions'].to_excel(writer, sheet_name='ActivityContributions', index=False)
-                data['pressure_contributions'].to_excel(writer, sheet_name='PressureContributions', index=False)
+                #
+                # export results
+                #
+                print('\tExporting results...', file=log)
+                temp_path = os.path.join(sim_res_path, f'sim_res_{i}.xlsx')
+                with pd.ExcelWriter(temp_path) as writer:
+                    data['pressure_levels'].to_excel(writer, sheet_name='PressureLevels', index=False)
+                    data['total_pressure_load_levels'].to_excel(writer, sheet_name='TPLLevels', index=False)
+                    data['total_pressure_load_reductions'].to_excel(writer, sheet_name='TPLReductions', index=False)
+                    data['thresholds']['PR'].to_excel(writer, sheet_name='RequiredReductionsForGES', index=False)
+                    data['measure_effects'].to_excel(writer, sheet_name='MeasureEffects', index=False)
+                    data['activity_contributions'].to_excel(writer, sheet_name='ActivityContributions', index=False)
+                    data['pressure_contributions'].to_excel(writer, sheet_name='PressureContributions', index=False)
 
-        except Exception as e:
-            fail_with_message(f'ERROR! Something went wrong (sim i = {i})! Check traceback.', e)
+            except Exception as e:
+                fail_with_message(f'ERROR! Something went wrong (sim i = {i})! Check traceback.', e)
     
     #
     # process results
@@ -130,8 +133,11 @@ def run(config_file: str = None):
 
 if __name__ == "__main__":
     config_file = None
+    skip_sim = False
     for i in range(len(sys.argv)):
         if sys.argv[i] in ['-config', '-c']:
             config_file = sys.argv[i+1]
-    run(config_file)
+        if sys.argv[i] in ['-skip', '-s']:
+            skip_sim = True
+    run(config_file, skip_sim)
 
